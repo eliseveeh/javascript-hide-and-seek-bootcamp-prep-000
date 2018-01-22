15a31fc9cc36c1c44f5fe99af53ea141dbc6f85a
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-22 03:07:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,27 @@ function nestedTarget(){
 }
 
 function increaseRankBy(n){
-  var ran = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < ran.list; i++){
-    var x = parseInt(ran[i].innerHTML)
-
-    ran[i].innerHTML = (x+1)
-    
+  var ran = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < ran.length; i++){
+    var x = parseInt(ran[i].innerHTML);
+    ran[i].innerHTML = (x + n).toString()
   }
+}
+
+function deepestChild(){
+  var current = document.querySelectorAll('div#grand-node div');
+  var next = [];
+
+  while (current){
+    if (typeof current.innerHTML === "string"){
+      return current.innerHTML;
+    }
+    if (Array.isArray(current)){
+      for (var x = 0; x < current.length; x++){
+        next.push(current[x])
+      }
+    }
+    current = next.shift()
+  }
+  return null;
 }
