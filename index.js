afc3637e25c354b8a072466e45b008b2be2bfc32
Elizabeth Veeh <elise.veeh@gmail.com>
2018-01-22 03:23:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n){
 
 function deepestChild(){
    var listOf = document.getElementById("grand-node").querySelectorAll('div');
-    return listOf.length
+    return listOf[3].innerHTML
 
 
 
